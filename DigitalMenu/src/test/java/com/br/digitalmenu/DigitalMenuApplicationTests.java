3e4fd101daf7459c0f87b0fc79c8b47e063ad2ab
Joao <joaovictorandradis@gmail.com>
2025-07-02 20:41:17 -0300

    Iniciando o projeto

---
+++ DigitalMenu/src/test/java/com/br/digitalmenu/DigitalMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.digitalmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
