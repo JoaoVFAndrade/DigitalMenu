a112679ab5771dc5106cb77954236fe9a963aded
João <joaovictorandradis0@gmail.com>
2025-08-20 20:47:24 -0300

    Adicionando algumas anotações nas entidades

--- DigitalMenu/src/main/java/com/br/digitalmenu/model/Ingrediente.java
+++ DigitalMenu/src/main/java/com/br/digitalmenu/model/Ingrediente.java
@@ -1,6 +1,8 @@
 package com.br.digitalmenu.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -11,10 +13,12 @@ public class Ingrediente {
     @Column(name = "id_ingrediente")
     private Long idIngrediente;
 
-    @Column(name = "nome_ingrediente")
+    @Column(name = "nome_ingrediente", length = 50)
+    @NotBlank
     private String nomeIngrediente;
 
     @Column(name = "em_estoque")
+    @NotNull
     private Integer estoque;
 
     private Boolean ativo;
--- DigitalMenu/src/main/java/com/br/digitalmenu/model/Restricao.java
+++ DigitalMenu/src/main/java/com/br/digitalmenu/model/Restricao.java
@@ -1,6 +1,7 @@
 package com.br.digitalmenu.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -13,6 +14,7 @@ public class Restricao {
     private Long idRestricao;
 
     @Column(unique = true,nullable = false, length = 50)
+    @NotBlank
     private String nomeRestricao;
 
     private Boolean ativo = true;
