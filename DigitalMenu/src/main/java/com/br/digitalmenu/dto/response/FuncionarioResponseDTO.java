fc36860783cfdeeeec3eaf3991757a31583cceaf
Joao <joaovictorandradis@gmail.com>
2025-08-23 12:52:57 -0300

    Entidade funcionario criada

---
+++ DigitalMenu/src/main/java/com/br/digitalmenu/dto/response/FuncionarioResponseDTO.java
@@ -0,0 +1,45 @@
+package com.br.digitalmenu.dto.response;
+
+import lombok.Data;
+
+import java.util.Set;
+
+
+public class FuncionarioResponseDTO {
+    private Long idFuncionario;
+    private String nome;
+    private String email;
+    private Set<String> roles;
+
+    public Long getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(Long idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
