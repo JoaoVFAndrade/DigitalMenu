90c8941dfeaebdcfe93d8a38658c62de52d37c78
JoaoVFAndrade <joaovictorandradis@gmail.com>
2025-09-05 00:42:38 -0300

    Finalizando o crud do pedido

---
+++ DigitalMenu/src/main/java/com/br/digitalmenu/model/Pedido.java
@@ -0,0 +1,37 @@
+package com.br.digitalmenu.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
+    private Long id;
+
+    @Column(name = "data_inicio")
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime abertoEm;
+
+    @Column(name = "data_final")
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime finalizadoEm;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private StatusPedido statusPedido;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal total;
+
+}
