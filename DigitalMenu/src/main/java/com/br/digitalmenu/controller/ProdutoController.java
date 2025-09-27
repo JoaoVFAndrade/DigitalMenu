240c8f371a054d77964dc6a474c259abff8cba80
pedro <psantosbrasileiro1@gmail.com>
2025-09-27 10:27:09 -0300

    criacao produto com foto

--- DigitalMenu/src/main/java/com/br/digitalmenu/controller/ProdutoController.java
+++ DigitalMenu/src/main/java/com/br/digitalmenu/controller/ProdutoController.java
@@ -2,12 +2,17 @@ package com.br.digitalmenu.controller;
 
 import com.br.digitalmenu.dto.request.ProdutoRequestDTO;
 import com.br.digitalmenu.dto.response.ProdutoResponseDTO;
+import com.br.digitalmenu.service.CloudinaryService;
 import com.br.digitalmenu.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.MediaType;
 
+
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,6 +21,9 @@ public class ProdutoController {
 
     @Autowired
     ProdutoService produtoService;
+
+    @Autowired
+    private CloudinaryService cloudinaryService;
 
     @GetMapping
     public ResponseEntity<List<ProdutoResponseDTO>> findAll(){
@@ -41,6 +49,13 @@ public class ProdutoController {
     public ResponseEntity<ProdutoResponseDTO> desativaProduto(@PathVariable Long id){
         produtoService.desativarProduto(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping(value = "/foto",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadFoto(
+            @RequestPart("file") MultipartFile file
+    ) throws IOException {
+        return ResponseEntity.ok(cloudinaryService.uploadFile(file));
     }
 
 }
