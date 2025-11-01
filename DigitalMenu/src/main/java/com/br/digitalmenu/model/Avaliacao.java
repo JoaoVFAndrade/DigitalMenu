9e460ca40d2efe8220e45e8beb1c734ad70e7fc0
pedro <psantosbrasileiro1@gmail.com>
2025-10-31 21:49:56 -0300

    entidade avaliacao

---
+++ DigitalMenu/src/main/java/com/br/digitalmenu/model/Avaliacao.java
@@ -0,0 +1,35 @@
+package com.br.digitalmenu.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Data
+public class Avaliacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAvaliacao;
+
+    @Column(nullable = false)
+    private Byte notaAtendimento;
+
+    @Column(nullable = false)
+    private Byte notaAmbiente;
+
+    @Column(nullable = false)
+    private Byte notaQualidadeDosProduto;
+
+    @Column(length = 50)
+    private String produto;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private LocalTime horario;
+
+}
