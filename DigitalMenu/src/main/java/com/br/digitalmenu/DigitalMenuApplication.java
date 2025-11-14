ed3169a6840a1d56f34dafc8a9564c374b70d7ca
pedro <psantosbrasileiro1@gmail.com>
2025-11-13 22:17:34 -0300

    geracao do cliente padrao

--- DigitalMenu/src/main/java/com/br/digitalmenu/DigitalMenuApplication.java
+++ DigitalMenu/src/main/java/com/br/digitalmenu/DigitalMenuApplication.java
@@ -1,7 +1,9 @@
 package com.br.digitalmenu;
 
+import com.br.digitalmenu.model.Cliente;
 import com.br.digitalmenu.model.Funcionario;
 import com.br.digitalmenu.model.RoleNome;
+import com.br.digitalmenu.repository.ClienteRepository;
 import com.br.digitalmenu.repository.FuncionarioRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -20,7 +24,7 @@ public class DigitalMenuApplication {
 	}
     //@Todo deixar esse comandline para poder acessar a aplicacao
     @Bean
-    CommandLineRunner init(FuncionarioRepository repo, PasswordEncoder encoder) {
+    CommandLineRunner init(FuncionarioRepository repo, PasswordEncoder encoder, ClienteRepository clienteRepository) {
         return args -> {
             if (repo.findByEmail("admin@email.com").isEmpty()) {
                 Funcionario admin = new Funcionario();
@@ -29,6 +33,17 @@ public class DigitalMenuApplication {
                 admin.setSenha(encoder.encode("admin123"));
                 admin.setRoles(Set.of(RoleNome.FUNCIONARIO_ADM));
                 repo.save(admin);
+
+                Cliente cliente = new Cliente();
+                cliente.setNome("clienteGarcom");
+                cliente.setSenha("hello");
+                cliente.setEmail("email@email.com");
+                cliente.setEmailValidado(true);
+                cliente.setRole(RoleNome.CLIENTE);
+                cliente.setDataNascimento(LocalDate.of(2000,1,1));
+                cliente.setRestricoes(new ArrayList<>());
+
+                clienteRepository.save(cliente);
 
                 System.out.println("✅ Usuário ADMIN criado com sucesso!");
             }
